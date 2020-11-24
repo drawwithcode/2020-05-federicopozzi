7099d9956203cf21c991569259fc15bd262da41b
Federico <federico.izzop@gmail.com>
2020-11-24 11:55:24 +0100

    new connection

--- public/sketch.js
+++ public/sketch.js
@@ -1,9 +1,14 @@
+let socket = io();
+
+
+
 function preload(){
   // put preload code here
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
+  background("peachpuff");
   // put setup code here
 }
 
